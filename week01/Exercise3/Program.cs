13adadd50938e9b5d2b4a7bbd2977f0c246d47b3
Telemundo123 <rukundodavis54@gmail.com>
2026-07-05 23:33:11 +0200

    Used the loop to request the user to again play whenever he didn't guess the magic number.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,23 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
         Console.Write("What is the magic number? ");
         int magicNumber = Convert.ToInt32(Console.ReadLine());
 
         Console.Write("What is your guess? ");
         int guess = Convert.ToInt32(Console.ReadLine());
 
-        if (guess > magicNumber)
+        while (guess != magicNumber)
         {
-            Console.WriteLine("Lower");
+            Console.WriteLine("Wrong Guess, Please Try again!");
+            Console.Write("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
         }
-        else if (guess < magicNumber)
-        {
-            Console.WriteLine("Higher");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!");
-        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
