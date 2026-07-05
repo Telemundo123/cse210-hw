8d0eaf002d8cdaf39798d1d33f214da4113dca26
Telemundo123 <rukundodavis54@gmail.com>
2026-07-05 23:11:08 +0200

    Magic number game, usind the intial Magic number entered by the user

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number? ");
+        int magicNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("What is your guess? ");
+        int guess = Convert.ToInt32(Console.ReadLine());
+
+        if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
