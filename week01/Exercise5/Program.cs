814d18becd9233811c52eac431f8f8e2899a1257
Telemundo123 <rukundodavis54@gmail.com>
2026-07-06 16:39:38 +0200

    Did Exercise 5, showcasing the use of functions in C#

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,47 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        DisplayWelcome();
+
+        String name = PromptUserName();
+
+        int number = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(number);
+
+        DisplayResult(name, squaredNumber);
+
     }
+    // Displaying a welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to a wonderful Program");
+    }
+    // Prompting the user for their name
+    static String PromptUserName()
+    {
+        Console.Write("What  is your name? ");
+        String name = Console.ReadLine();
+        return name;
+    }
+    // Prompting the user for a number
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number?");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    // Squaring the number
+    static int SquareNumber(int number)
+    {
+        int squaredNumber = number * number;
+        return squaredNumber;
+    }
+    // Displaying the results
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"Brother {name}, the square of your number is {squaredNumber}");
+    }
+
 }
